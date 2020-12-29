7fabf83121c2118efd667ef6967481a349d2e1af
andylamax <andylamax@programmer.net>
2020-12-29 12:39:38 +0300

    Added CompletableFuture.asLater()

--- later-core/src/jvmMain/kotlin/tz/co/asoft/Later.core.jvm.ktx.kt
+++ later-core/src/jvmMain/kotlin/tz/co/asoft/Later.core.jvm.ktx.kt
@@ -1,6 +1,7 @@
 package tz.co.asoft
 
 import kotlinx.coroutines.*
+import java.util.concurrent.CompletableFuture
 
 val LATER_DISPATCHER_JVM: ExecutorCoroutineDispatcher = newSingleThreadContext("tz.co.asoft.LATER_DISPATCHER")
 
@@ -8,4 +9,15 @@ val LATER_SCOPE_JVM = CoroutineScope(SupervisorJob() + LATER_DISPATCHER_JVM)
 
 actual fun loadToNextEventLoop(body: () -> Unit) {
     LATER_SCOPE_JVM.launch { body() }
+}
+
+fun <T> CompletableFuture<T>.asLater(): Later<T> = Later { resolve, reject ->
+    whenComplete { value: T?, err: Throwable? ->
+        when {
+            value != null && err != null -> resolve(value)
+            value != null && err == null -> resolve(value)
+            value == null && err != null -> reject(err)
+            value == null && err == null -> reject(IllegalStateException("Completable future didn't return with value or exception"))
+        }
+    }
 }
